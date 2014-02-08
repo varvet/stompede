55af34db7d880c000b8772734200995353b98cd5
Kim Burgestrand <kim@burgestrand.se>
2014-02-08 13:32:03 +0100

    Move parser specs to stomp_spec to reflect where the files are loaded

--- spec/stompede/stomp_spec.rb
+++ spec/stompede/stomp_spec.rb
@@ -1,2 +1,11 @@
-describe Stompede::Stomp do
+describe Stompede::Stomp::RubyParser do
+  it_behaves_like "a stompede parser"
+end
+
+if defined?(Stompede::Stomp::CParser)
+  describe Stompede::Stomp::CParser do
+    pending "not implemented" do
+      it_behaves_like "a stompede parser"
+    end
+  end
 end
