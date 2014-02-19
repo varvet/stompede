61ed1546dbf4fde9fe9fe1ba9c655b2df1d6ada8
Jonas Nicklas <jonas.nicklas@gmail.com>
2014-02-19 09:22:57 +0100

    More specs for heartbeats

--- spec/stompede/heartbeats_spec.rb
+++ spec/stompede/heartbeats_spec.rb
@@ -25,20 +25,76 @@ describe Stompede::Stomplet do
         app.should_not be_alive
       end
 
-      it "does not send heart beats when client doesn't want to receive them"
-      it "does not receive heart beats when client doesn't want to send them"
-      it "lets client configure when to receive heart beats"
-      it "lets client configure when to send heart-beats"
+      it "does not send heart beats when client doesn't want to receive them" do
+        send_message(client_io, "CONNECT", "accept-version" => Stompede::STOMP_VERSION, "heart-beat" => "0,0")
+        parse_message(client_io).command.should == "CONNECTED"
+        sleep 0.008
+        client_io.should be_an_empty_socket
+      end
+
+      it "does not receive heart beats when client doesn't want to send them" do
+        send_message(client_io, "CONNECT", "accept-version" => Stompede::STOMP_VERSION, "heart-beat" => "0,2")
+        parse_message(client_io).command.should == "CONNECTED"
+        sleep 0.008
+        app.should be_alive
+      end
+
+      it "lets client configure when to receive heart beats" do
+        send_message(client_io, "CONNECT", "accept-version" => Stompede::STOMP_VERSION, "heart-beat" => "0,10")
+        parse_message(client_io).command.should == "CONNECTED"
+        sleep 0.008
+        client_io.should be_an_empty_socket
+        client_io.readpartial(1000).should == "\n"
+      end
+
+      it "lets client configure when to send heart-beats" do
+        send_message(client_io, "CONNECT", "accept-version" => Stompede::STOMP_VERSION, "heart-beat" => "10,0")
+        parse_message(client_io).command.should == "CONNECTED"
+        sleep 0.008
+        app.should be_alive
+        sleep 0.003
+        app.should_not be_alive
+      end
     end
 
     context "when connector requires heart beats" do
-      let(:connector) { Stompede::Connector.new(app_klass, heart_beats: [0.005, 0.005], require_heart_beats: true, ack_modes: [:auto]) }
+      let(:connector) { Stompede::Connector.new(app_klass, heart_beats: [0.005, 0.005], require_heart_beats: true) }
 
-      it "sends heart-beats at regular intervals"
-      it "receives heart-beats at regular intervals"
-      it "does not send heart beats when client doesn't want to receive them"
+      it "sends heart-beats at regular intervals" do
+        send_message(client_io, "CONNECT", "accept-version" => Stompede::STOMP_VERSION, "heart-beat" => "0,2")
+        parse_message(client_io).command.should == "CONNECTED"
+        client_io.readpartial(1000).should == "\n"
+        client_io.readpartial(1000).should == "\n"
+      end
+
+      it "receives heart-beats at regular intervals" do
+        send_message(client_io, "CONNECT", "accept-version" => Stompede::STOMP_VERSION, "heart-beat" => "2,0")
+        parse_message(client_io).command.should == "CONNECTED"
+        client_io.write("\n")
+        sleep 0.003
+        client_io.write("\n")
+        app.should be_alive
+        sleep 0.008
+        app.should_not be_alive
+      end
+
+      it "does not send heart beats when client doesn't want to receive them" do
+        send_message(client_io, "CONNECT", "accept-version" => Stompede::STOMP_VERSION, "heart-beat" => "0,0")
+        parse_message(client_io).command.should == "CONNECTED"
+        sleep 0.008
+        client_io.should be_an_empty_socket
+      end
+
       it "raises a client error when client doesn't want to send heart beats"
-      it "lets client configure when to receive heart beats"
+
+      it "lets client configure when to receive heart beats" do
+        send_message(client_io, "CONNECT", "accept-version" => Stompede::STOMP_VERSION, "heart-beat" => "0,10")
+        parse_message(client_io).command.should == "CONNECTED"
+        sleep 0.008
+        client_io.should be_an_empty_socket
+        client_io.readpartial(1000).should == "\n"
+      end
+
       it "raises a client error when client doesn't want to send heart beats often enough"
     end
   end
